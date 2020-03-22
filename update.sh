95a5ec5bfee3a9f3bafbf7775c1bfc902f1f293f
Emile Courthoud <emilec@student.ethz.ch>
2020-03-22 15:52:55 +0100

    New update

--- update.sh
+++ update.sh
@@ -8,14 +8,10 @@
 # Give name to the commit
 read -p "Enter commit description: " description
 
-# Remove public directory if it exists
-#echo
-#rm -r public/
-#git submodule add -f -b master https://github.com/emilecourthoud/emilecourthoud.github.io.git public
-
 # Commit code
 echo
 git add .
+
 if [ -z $description ]
 then
     echo Empty message. Commit with title "New update".
@@ -24,20 +20,4 @@ else
     git commit -m $description
 fi
 
-
-
 git push -u origin master
-
-# Update websitels
-#echo
-#hugo
-
-# Publish website
-#cd public
-#git add .
-#git commit -m "Update website"
-#git push origin master
-
-
-#hugo server -D
-#http://localhost:1313/
