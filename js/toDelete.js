1fb464da60a615f03db8bfba9fcccfe63d45de63
Emile Courthoud <emilec@student.ethz.ch>
2020-04-03 00:21:37 +0200

    toDelete

--- js/toDelete.js
+++ js/toDelete.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
       if ($(document).scrollTop() > 10) { // check if user scrolled more than 50 from top of the browser window
         $(".navbar-fixed-top").css("background-color", "#f8f8f8"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
